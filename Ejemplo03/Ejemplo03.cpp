cf9646f4b934db7fa09f3379c0f3d4b06083edf2
Edson Contreras <edson.contreras113@gmail.com>
2022-05-12 14:01:21 -0500

    Agregar archivos de proyecto.

---
+++ Ejemplo03/Ejemplo03.cpp
@@ -0,0 +1,94 @@
+// Ejemplo_Moanso.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+// Ingresa n personas (nombres, edad, estatura) a una lista enlazada doble y
+// muÃ©stralos hacia atrÃ¡s empezando por el Ãºltimo. Las personas se registran al final
+// de la listaDoble.
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Person {
+    string name;
+    int age;
+    double height;
+};
+
+struct Nodo {
+    Person dato;
+    Nodo* ant;
+    Nodo* sgte;
+};
+
+void insertarFinal(Nodo*&, Person);
+void listar(Nodo*);
+
+int main(void) {
+    Nodo* listaDoble = NULL;
+
+    int cant;
+    Person valor;
+
+    cout << "LISTA DOBLE" << endl;
+    do {
+        cout << "Cantidad de personas a ingresar: "; cin >> cant;
+    } while (cant <= 0);
+
+    for (int i = 0; i < cant; i++) {
+        cout << "\n\nDATOS PERSONA [" << i + 1 << "]" << endl;
+        cout << "\nNombre: "; cin.ignore();
+        getline(cin, valor.name);
+        cout << "Edad: ";
+        cin >> valor.age;
+        cout << "Estatura: ";
+        cin >> valor.height; cout << endl;
+
+        insertarFinal(listaDoble, valor);
+    }
+
+    cout << "\n\n\nElementos de la lista: " << endl;
+    listar(listaDoble);
+
+    return 0;
+}
+
+void insertarFinal(Nodo*& listaDoble, Person valor)
+{
+    Nodo* nuevoNodo, * ultimo;
+    nuevoNodo = new Nodo;
+
+    nuevoNodo->dato = valor;
+    nuevoNodo->sgte = NULL;
+
+    if (listaDoble == NULL) {
+        nuevoNodo->ant = NULL;
+        listaDoble = nuevoNodo;
+    }
+    else {
+        ultimo = listaDoble;
+
+        while (ultimo->sgte != NULL) {
+            ultimo = ultimo->sgte;
+        }
+
+        ultimo->sgte = nuevoNodo;
+        nuevoNodo->ant = ultimo;
+    }
+}
+
+void listar(Nodo* listaDoble)
+{
+    Nodo* ultimo = listaDoble, * actual;
+
+    while (ultimo->sgte != NULL) {
+        ultimo = ultimo->sgte;
+    }
+    actual = ultimo;
+    while (actual != NULL) {
+        cout << actual->dato.name << endl;
+        cout << actual->dato.age << endl;
+        cout << actual->dato.height << endl;
+        cout << endl << endl;
+        actual = actual->ant;
+    }
+}
